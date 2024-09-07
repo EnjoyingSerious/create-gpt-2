95f81a9d7568dff4e8157c9f170d4fe7f66a8984
striveZ <zgy011122@163.com>
2024-09-07 22:10:54 +0800

    根据论文完成了对权重初始化的要求，包括 1. 线性层权重的初始化 2. 嵌入层权重的初始化 3. 残差块对标准差的影响

--- train_gpt-2.py
+++ train_gpt-2.py
@@ -14,6 +14,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         #output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         
         self.n_embd = config.n_embd
         self.n_head = config.n_head
@@ -42,7 +43,8 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate = "tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
-    
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+        
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
@@ -94,6 +96,19 @@ class GPT(nn.Module):
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
         
+        # init params
+        self.apply(self._init_weights)
+        
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** (-0.5)
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets = None):
         B, T = idx.size()
